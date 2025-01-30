ad210c1d1d7c913715a8d7c5b8628b104b17965c
Vinicius <vinicius.gdechechi@gmail.com>
2025-01-30 12:39:41 -0300

    consumo da api no list component

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import axios from 'axios';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
 
+  products: any[] = []; // Lista de produtos
+
+  // Injetamos o HttpClient no componente
+  private httpClient = inject(HttpClient);
+
+  ngOnInit(){
+    axios.get('/api/products').then((response) => {
+      
+      this.products = response.data;
+
+    }).catch((error) => {
+
+      console.log(error);
+    })
+  };
 }
+
