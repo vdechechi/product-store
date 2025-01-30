560c37a16ed311d7540f4b4775e86c6c82a2da29
Vinicius <vinicius.gdechechi@gmail.com>
2025-01-30 14:18:49 -0300

    adiciona link para criar produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -6,11 +6,13 @@ import { Product } from '../../shared/interfaces/product.interface';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
